ed4e8c09c21f8adb25ee84e8f0a18c52053c9eb2
Duy Nguyen <duynn17@fsoft.com.vn>
2022-12-12 19:07:35 +0700

    commit

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Password1994#
-export POSTGRES_HOST=database-1.cywcoqz01byo.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.c4kgdctaupok.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=project-3-duynn17
 export AWS_REGION=us-east-1
