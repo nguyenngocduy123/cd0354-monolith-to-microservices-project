fdfc457e5b114eb03567cd10c1bbe0b8f8fd3459
Duy Nguyen <duynn17@fsoft.com.vn>
2022-12-13 00:14:46 +0700

    commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a344bf0a044e542ea85a160c5437e5b3-883190169.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
